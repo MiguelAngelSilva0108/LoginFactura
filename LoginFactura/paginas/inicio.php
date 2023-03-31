186b82a09bb5288c8abc73adb59e85df09352f89
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-03-31 07:10:14 -0600

    Add files via upload

--- LoginFactura/paginas/inicio.php
+++ LoginFactura/paginas/inicio.php
@@ -1,22 +1,4 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
-}
 
-require './database/database.php';
-
-$records = $conn->prepare('SELECT id_users, email, password FROM users WHERE id_users = :id_users');
-$records->bindParam(':id_users', $_SESSION['user_id']);
-$records->execute();
-$results = $records->fetch(PDO::FETCH_ASSOC);
-
-$user = null;
-
-if (count($results) > 0) {
-    $user = $results;
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,32 +11,19 @@ if (count($results) > 0) {
 </head>
 <?PHP require('./navbar/navbar.php');
 ?>
-<link rel="stylesheet" href="./css/bootstrap.min.css" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
 <link rel="stylesheet" href="./css/navbar.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Dependencias de jQuery y Popper.js -->
-<script src="./js/jquery-3.6.0.min.js"></script>
-<script src="./js/popper.min.js"></script>
 
-<!-- Scripts de Bootstrap -->
-<script src="./js/bootstrap.min.js"></script>
 
 <body>
     <div>
-
         <div class="img-container">
             <img class="img-fluid"
                 src="https://izzi.telmov.mx/pub/media/amasty/webp/wysiwyg/Banners_Vtex_1290x480_1_-10.webp" alt="..." />
         </div>
-        <?php if (!empty($user)): ?>
-            <p>Bienvenido
-                <?= $user['email'] ?>
-            </p>
-            <form action="/logout.php" method="POST">
-                <input type="submit" value="Cerrar sesión">
-            </form>
-        <?php endif; ?>
     </div>
 </body>
 
--- LoginFactura/paginas/login.php
+++ LoginFactura/paginas/login.php
@@ -1,25 +1,7 @@
 <?php
-session_start();
-if (isset($_SESSION['user_id'])) {
-  header('Location: inicio.php');
-}
 
-require './database/database.php';
 
-$message = '';
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $records = $conn->prepare('SELECT id_users, email, password FROM users WHERE email = :email');
-  $records->bindParam(':email', $_POST['email']);
-  $records->execute();
-  $results = $records->fetch(PDO::FETCH_ASSOC);
 
-  if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-    $_SESSION['user_id'] = $results['id_users'];
-    header('Location: inicio.php');
-  } else {
-    $message = 'Sus credenciales son erróneas';
-  }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,9 +23,9 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
 <body>
     <?PHP require('../navbar/navbar.php');
     ?>
-    <?php if (!empty($message)): ?>
-    <p><?= $message ?></p>
-  <?php endif; ?>
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
     <div class="Login">
 
         <!--Resto del código de la página de inicio de sesión-->
