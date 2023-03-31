0f35896403b83ea43ca080941c131b9a12a7052f
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-03-31 05:23:37 -0600

    Add files via upload

--- LoginFactura/database/database.php
+++ LoginFactura/database/database.php
@@ -3,10 +3,13 @@
 $server = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'php-factura';
+$database = 'php_login_database';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+  $stmt = $conn->query("SELECT * FROM tabla_prueba");
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  print_r($rows);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
