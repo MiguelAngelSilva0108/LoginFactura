88cba2c0b295d0b36189776d9e1692780898b718
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-03-31 00:44:46 -0600

    Update inicio.php

--- LoginFactura/paginas/inicio.php
+++ LoginFactura/paginas/inicio.php
@@ -6,13 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Izzi México</title>
-    
 
 </head>
-<?PHP require('./navbar/navbar.php');
-?>
+<?php require_once('../navbar/navbar.php'); ?>
+
 <link rel="stylesheet" href="./css/bootstrap.min.css"/>
 <link rel="stylesheet" href="./css/navbar.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
 <!-- Dependencias de jQuery y Popper.js -->
 <script src="./js/jquery-3.6.0.min.js"></script>
 <script src="./js/popper.min.js"></script>
