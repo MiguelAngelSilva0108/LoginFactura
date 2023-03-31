3ece436d2e3fe17354a180136878a4921f834b1d
MiguelAngelSilva0108 <103139025+MiguelAngelSilva0108@users.noreply.github.com>
2023-03-31 00:57:00 -0600

    Update navbar.php

--- LoginFactura/navbar/navbar.php
+++ LoginFactura/navbar/navbar.php
@@ -1,23 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="./paginas/inicio.php">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/Logotipo_izzi_negativo.png" alt="Logo" width="120"
-            height="50" class="d-inline-block align-text-top" href="./paginas/inicio.php" />
+        <a class="navbar-brand" href="../index.php">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/Logotipo_izzi_negativo.png" alt="Logo"
+                width="120" height="50" class="d-inline-block align-text-top" href="./paginas/inicio.php" />
         </a>
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="login.php">
-                        Iniciar sesión
-                        </Link>
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                 </li>
-
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Iniciar sesión</a>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
@@ -25,12 +23,7 @@
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#">Crear Factura</a></li>
-                        <li><a class="dropdown-item" href="#">Revisar Factura</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="#">Contacto</a></li>
-
+                        <li><a class="dropdown-item" href="#">Consultar Factura</a></li>
                     </ul>
                 </li>
             </ul>
